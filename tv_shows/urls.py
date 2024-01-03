1f4f235ed24a8fa369e5d85f4470f6d163ece9a4
Bermet0 <bekatktbkv@gmail.com>
2024-01-04 00:33:51 +0600

    ДЗ-5

--- tv_shows/urls.py
+++ tv_shows/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('movie_list/', views.movie_list, name='movie_list'),
-    path('movie_list/<int:id>/', views.movie_detail, name='movie_detail'),
-    path('create_movie/', views.movie_create_view, name='create_movie'),
+    path('movie_list/', views.MovieListView, name='movie_list'),
+    path('movie_list/<int:id>/', views.MovieDetailView, name='movie_detail'),
+    path('create_movie/', views.MovieCreateView, name='create_movie'),
     path('movie_list_delete/', views.movie_list_delete_view, name='movie_list_delete'),
-    path('movie_drop/<int:id>/delete/', views.movie_drop_view, name='movie_drop'),
+    path('movie_drop/<int:id>/delete/', views.MovieDropView, name='movie_drop'),
     path('movie_list_update/', views.movie_list_edit_view, name='movie_list_update'),
-    path('movie_update/<int:id>/update/', views.movie_update, name='movie_update'),
-    path('movie_comm/<int:id>/', views.movie_comm, name='movie_comm'),
-]
+    path('movie_update/<int:id>/update/', views.MovieUpdateView, name='movie_update'),
+    # path('movie_comm/<int:id>/', views.movie_comm, name='movie_comm'),
+    path('search/', views.SearchView.as_view(), name='search'),
+    ]
+
+
--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,35 +1,61 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import UpdateView
 from . import models, forms
 from django.urls import reverse
 from django.http import HttpResponse
 from .forms import ReviewForm
+from django.views import generic
 
 
 # Create your views here.
-def movie_list(request):
-    if request.method == 'GET':
-        movies = models.Movie.objects.all()
-        return render(request, template_name='movies/movie_list.html',
-                      context={'movie': movies})
+# def movie_list(request):
+#     if request.method == 'GET':
+#         movies = models.Movie.objects.all()
+#         return render(request, template_name='movies/movie_list.html',
+#                       context={'movie': movies})
+class MovieListView(generic.ListView):
+    template_name = 'movies/movie_list.html'
+    model = models.Movie
+    def get_queryset(self):
+        return self.model.objects.all()
 
 
-def movie_detail(request, id):
-    if request.method == 'GET':
-        movie_id = get_object_or_404(models.Movie, id=id)
-        return render(request, template_name='movies/movie_detail.html',
-                      context={'movie_id': movie_id})
+# def movie_detail(request, id):
+#     if request.method == 'GET':
+#         movie_id = get_object_or_404(models.Movie, id=id)
+#         return render(request, template_name='movies/movie_detail.html',
+#                       context={'movie_id': movie_id})
 
 
-def movie_create_view(request):
-    if request.method == 'POST':
-        form = forms.MovieForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('movie_list'))
-    else:
-        form = forms.MovieForm()
-        return render(request, template_name='crud/create_movie.html',
-                      context={'form': form})
+class MovieDetailView(generic.DetailView):
+    template_name = 'movies/movie_detail.html'
+    context_object_name = 'movie_id'
+    def get_object(self, **kwargs):
+        movie_id = self.kwargs.get('id')
+        return get_object_or_404(models.Movie, id=movie_id)
+
+
+# def movie_create_view(request):
+#     if request.method == 'POST':
+#         form = forms.MovieForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse('movie_list'))
+#     else:
+#         form = forms.MovieForm()
+#         return render(request, template_name='crud/create_movie.html',
+#                       context={'form': form})
+
+
+class MovieCreateView(generic.CreateView):
+    template_name = 'crud/movie_create.html'
+    model = models.Movie
+    form_class = forms
+    success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(MovieCreateView, self).form_valid(form=form)
 
 
 def movie_list_delete_view(request):
@@ -39,11 +65,20 @@ def movie_list_delete_view(request):
                       context={'movie_delete': movies_delete})
 
 
-def movie_drop_view(request, id):
-    movie_id = get_object_or_404(models.Movie, id=id)
-    movie_id.delete()
-    # return HttpResponse('Успешно удален')
-    return redirect(reverse('movie_list_delete'))
+# def movie_drop_view(request, id):
+#     movie_id = get_object_or_404(models.Movie, id=id)
+#     movie_id.delete()
+#     # return HttpResponse('Успешно удален')
+#     return redirect(reverse('movie_list_delete'))
+
+
+class MovieDropView(generic.DeleteView):
+    template_name = 'crud/delete/confirm_delete.html'
+    success_url = 'movie_list_delete'
+
+    def get_object(self, **kwargs):
+        movie_id = self.kwargs.get('id')
+        return get_object_or_404(models.Movie, id=movie_id)
 
 
 def movie_list_edit_view(request):
@@ -53,35 +88,62 @@ def movie_list_edit_view(request):
                       context={'movie_update': movies_update})
 
 
-def movie_update(request, id):
-    movie_id = get_object_or_404(models.Movie, id=id)
-    if request.method == 'POST':
-        form = forms.MovieForm(instance=movie_id, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('movie_list_update'))
-    else:
-        form = forms.MovieForm(instance=movie_id)
-        return render(request, template_name='crud/update/movie_update.html',
-                      context={
-                          "form": form,
-                          "movie_id": movie_id
-                      })
+# def movie_update(request, id):
+#     movie_id = get_object_or_404(models.Movie, id=id)
+#     if request.method == 'POST':
+#         form = forms.MovieForm(instance=movie_id, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse('movie_list_update'))
+#     else:
+#         form = forms.MovieForm(instance=movie_id)
+#         return render(request, template_name='crud/update/movie_update.html',
+#                       context={
+#                           "form": form,
+#                           "movie_id": movie_id
+#                       })
 
 
-def movie_comm(request, movie_id):
-    movie = models.Movie.objects.get(id=movie_id)
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.movie = movie
-            review.save()
-    else:
-        form = ReviewForm()
+class MovieUpdateView(generic.UpdateView):
+    template_name = 'crud/update/movie_list_update.html'
+    form_class = forms.MovieForm
+    success_url = 'movie_list_update'
 
-    return render(request, template_name='movie_detail.html',
-                  context={
-                      'movie': movie,
-                      'form': form
-                  })
+    def get_object(self, **kwargs):
+        movie_id = self.kwargs.get('id')
+        return get_object_or_404(models.Movie, id=movie_id)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(MovieUpdateView, self).form_valid(form=form)
+
+
+# def movie_comm(request, movie_id):
+#     movie = models.Movie.objects.get(id=movie_id)
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             review = form.save(commit=False)
+#             review.movie = movie
+#             review.save()
+#     else:
+#         form = ReviewForm()
+#
+#     return render(request, template_name='movie_detail.html',
+#                   context={
+#                       'movie': movie,
+#                       'form': form
+#                   })
+
+
+class SearchView(generic.ListView):
+    template_name = 'movies/movie_list.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return models.Movie.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
